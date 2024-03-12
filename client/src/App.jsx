65af2dc756cfc27141fc5af2cbbd4d8426262d01
Eduard Saadyan <eduard@Eduards-MacBook-Pro.local>
2024-03-12 11:14:52 +0100

    Added css reset, color variables and font

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GlobalCSS from "./styles/app.module.scss";
+
+const App = function () {
+  return <div className={GlobalCSS.app}>Hello!</div>;
+};
+
+export default App;
---
+++ client/src/pages/Suggestions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Suggestions = function () {
+  return <></>;
+};
+
+export default Suggestions;
